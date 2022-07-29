2c4354232ad61bb53aaee8612be1f1293d9a84f7
Jan Engelhardt <jengelh@inai.de>
2022-07-29 21:26:06 +0200

    do-admin-cli: avoid use of /tmp during rst preparation

--- admin/do-admin-cli.sh
+++ admin/do-admin-cli.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-if [ -d "/tmp/admin-api" ]; then
-	rm -rf /tmp/admin-api
-fi
-mkdir -p /tmp/admin-api
+rm -Rf .aapi
+git clone --depth=2 https://github.com/grommunio/admin-api.git .aapi
 
-git clone --depth=2 https://github.com/grommunio/admin-api.git /tmp/admin-api
-
-cat /tmp/admin-api/doc/rst/grommunio-admin.rst /tmp/admin-api/doc/rst/grommunio-admin-*.rst /tmp/admin-api/doc/rst/grommunio-dbconf.rst
+cat .aapi/doc/rst/grommunio-admin.rst .aapi/doc/rst/grommunio-admin-*.rst .aapi/doc/rst/grommunio-dbconf.rst
 
 if [ -e "admin-api.rst" ]; then
 	rm -f admin-api.rst
 fi
 
-for rst in /tmp/admin-api/doc/rst/grommunio-admin-*.rst /tmp/admin-api/doc/rst/grommunio-dbconf.rst; do
+for rst in .aapi/doc/rst/grommunio-admin-*.rst .aapi/doc/rst/grommunio-dbconf.rst; do
 	sed -i '1s/.*//' $rst
 	(
 	sed '3!b;s/=/\&/g' $rst | sed 's#(1)##' |
@@ -22,4 +18,3 @@ for rst in /tmp/admin-api/doc/rst/grommunio-admin-*.rst /tmp/admin-api/doc/rst/g
 	perl -lpe 's{^&{3,}}{"-" x length($&)}e' | sed '/^See Also$/,$d'
 	) >>admin-api.rst
 done
-
