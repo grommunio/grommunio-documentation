c21bc988bf18ee125e32db6ddc360a034cf13850
Jan Engelhardt <jengelh@inai.de>
2022-07-29 15:36:19 +0200

    generate-man-pages: avoid use of /tmp during roff-to-rst conversion
    /tmp is where everyone can write (and because no mktemp is used,
    break and interfere with our invocation of generate-man-pages.sh).
    /tmp eschews potential disk quotas in `.`.
    /tmp is space-constrained on 2022-era systems.

--- man/generate-man-pages.sh
+++ man/generate-man-pages.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-if [ -d "/tmp/gromox" ]; then
-	rm -rf /tmp/gromox
-fi
-mkdir -p /tmp/gromox
-if [ -d "/tmp/admin-api" ]; then
-	rm -rf /tmp/admin-api
-fi
-mkdir -p /tmp/admin-api
-
-git clone --depth=2 https://github.com/grommunio/admin-api.git /tmp/admin-api
-git clone --depth=1 https://github.com/grommunio/gromox.git /tmp/gromox
-pushd /tmp/gromox/doc
+rm -Rf .aapi .gromox
+git clone --depth=2 https://github.com/grommunio/admin-api.git .aapi
+git clone --depth=1 https://github.com/grommunio/gromox.git .gromox
+pushd .gromox/doc/
 	rm -f *.rst
 	for i in *.[0-9]*; do
 	       pandoc -s -f man -t rst -o "$i.rst" "$i";
@@ -20,6 +12,6 @@ popd
 
 rm -f gromox.7.rst gromox-pffimport.8.rst *gx*.rst
 
-mv /tmp/gromox/doc/*.rst .
-sed -i 's#(1)##g' /tmp/admin-api/doc/rst/*.rst
-mv /tmp/admin-api/doc/rst/*.rst .
+mv .gromox/doc/*.rst .
+sed -i 's#(1)##g' .aapi/doc/rst/*.rst
+mv .aapi/doc/rst/*.rst .
