1853522a3396eac771942166e44625edb68381f3
Jan Engelhardt <jengelh@inai.de>
2022-07-29 20:27:25 +0200

    generate-man-pages: avoid in-place modifications of cloned subrepos

--- man/generate-man-pages.sh
+++ man/generate-man-pages.sh
@@ -3,14 +3,17 @@
 rm -Rf .aapi .gromox
 git clone --depth=2 https://github.com/grommunio/admin-api.git .aapi
 git clone --depth=1 https://github.com/grommunio/gromox.git .gromox
+
+rm -f gromox.7.rst gromox-pffimport.8.rst *gx*.rst grommunio-admin-*.rst
 pushd .gromox/doc/
-	rm -f *.rst
 	for i in *.[0-9]*; do
-	       pandoc -s -f man -t rst -o "$i.rst" "$i";
+	       pandoc -s -f man -t rst -o "$OLDPWD/$i.rst" "$i";
 	done
 popd
 
-rm -f gromox.7.rst gromox-pffimport.8.rst *gx*.rst grommunio-admin-*.rst
-mv .gromox/doc/*.rst .
-sed -i 's#(1)##g' .aapi/doc/rst/*.rst
-mv .aapi/doc/rst/*.rst .
+pushd .aapi/doc/rst/
+	for i in *.rst; do
+		echo "$i -> $OLDPWD/$i"
+		sed -e 's#(1)##g' <"$i" >"$OLDPWD/$i"
+	done
+popd
